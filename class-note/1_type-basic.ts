d0ce867bcbefeda47a8b36fd2ca5b5c2d8b9fb1b
myungsangBaek <bms6168@naver.com>
2022-07-09 19:08:20 +0900

    [FEATURE] Declare string,number,array

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+// var str = "hello";
+
+// TS 문자열 선언
+const str: string = "hello";
+
+// TS 숫자 선언
+const num: number = 10;
+
+// TS 배열 선언
+const arr: Array<number> = [1, 2, 3];
+const dododot: Array<string> = ["Lil", "Stark", "Robert", "Han"];
+const items: number[] = [1, 2, 3];
