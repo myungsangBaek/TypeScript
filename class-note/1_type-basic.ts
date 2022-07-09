8f4854f6c93240a1ebdeb1a8bb2d2995894986de
myungsangBaek <bms6168@naver.com>
2022-07-09 19:15:52 +0900

    [FEATURE] Basic type - tuple, object, boolean

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -11,3 +11,21 @@ const num: number = 10;
 const arr: Array<number> = [1, 2, 3];
 const dododot: Array<string> = ["Lil", "Stark", "Robert", "Han"];
 const items: number[] = [1, 2, 3];
+
+// TS 튜플
+const address1: [string, number] = ["gangnam", 123];
+
+// TS 객체
+const obj: object = {};
+const person: object = {
+  name: "lil",
+  age: 100,
+};
+
+const person1: { name: string; age: number } = {
+  name: "han",
+  age: 300,
+};
+
+// TS 진위값
+const show: boolean = true;
