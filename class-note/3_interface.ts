5e8adce940b3a22c58e69aba3ab7fdc43e5bb308
myungsangBaek <bms6168@naver.com>
2022-07-14 20:43:10 +0900

    [FEATURE] Interface that defines the factors of the function

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,20 @@ interface User {
   name: string;
 }
 
+//변수에 인터페이스 활용
+
 const seho: User = {
   age: 29,
   name: "한",
 };
+
+//함수에 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+
+const capt = {
+  name: "한",
+  age: 100,
+};
+getUser(capt);
