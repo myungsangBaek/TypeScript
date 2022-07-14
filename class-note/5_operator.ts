c53034495797ba6eb03f42af4f82cbd05bf0dfb9
myungsangBaek <bms6168@naver.com>
2022-07-14 21:21:25 +0900

    [FEATURE] Characteristics of union type

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -5,7 +5,7 @@ function logMessage(value: any) {
 logMessage("hello");
 logMessage(100);
 
-const tata: string | number | boolean;
+// const tata: string | number | boolean;
 
 function logMessage1(value: string | number) {
   if (typeof value === "number") {
@@ -14,7 +14,22 @@ function logMessage1(value: string | number) {
   if (typeof value === "string") {
     value.toString();
   }
-  throw new TypeError('value must be string or number';)
+  throw new TypeError("value must be string or number");
 }
 logMessage("hello");
 logMessage(100);
+
+interface Developer2 {
+  name: string;
+  skill: string;
+}
+
+interface Person2 {
+  name: string;
+  age: number;
+}
+
+//공통된 속성만 접근 가능
+function askSomeone(someone: Developer2 | Person2) {
+  someone.name;
+}
