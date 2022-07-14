3fb6fca47ddceb8b519cc612a7878b121c30a6ab
myungsangBaek <bms6168@naver.com>
2022-07-14 21:16:59 +0900

    [FEATURE] Strength union type

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -5,8 +5,16 @@ function logMessage(value: any) {
 logMessage("hello");
 logMessage(100);
 
+const tata: string | number | boolean;
+
 function logMessage1(value: string | number) {
-  console.log(value);
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError('value must be string or number';)
 }
 logMessage("hello");
 logMessage(100);
