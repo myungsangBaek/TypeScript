c415bd7201ed248cc2ccff327770a494f027f168
myungsangBaek <bms6168@naver.com>
2022-07-14 20:47:41 +0900

    [FEATURE] Define function structure

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -20,3 +20,18 @@ const capt = {
   age: 100,
 };
 getUser(capt);
+
+//함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+let sum: SumFunction;
+sum = function (a: number, b: number): number {
+  return a + b;
+};
+
+//인덱싱
+interface StringArray {
+  a;
+}
