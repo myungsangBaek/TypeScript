045b7c03716389480e810134e4fe7faee9e52fa5
myungsangBaek <bms6168@naver.com>
2022-07-16 22:00:14 +0900

    [FEATURE] Class syntax about typescript

--- class-note/class.js
+++ class-note/class.js
@@ -1,3 +1,10 @@
+function Person2(name, age) {
+  this.name = name;
+  this.age - age;
+}
+
+const capt = new Person2("캡틴", 100);
+
 class Person {
   constructor(name, age) {
     console.log("생성 되었습니다.");
