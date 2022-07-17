b920bdab7324d7532d889e2573879eb50ac32b9b
myungsangBaek <bms6168@naver.com>
2022-07-17 11:12:04 +0900

    [FEATURE] Declare generics in interface

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,24 +1,32 @@
-function logText(text: string) {
-  console.log(text);
-  text.split("").reverse().join("");
-  return text;
-}
+// function logText(text: string) {
+//   console.log(text);
+//   text.split("").reverse().join("");
+//   return text;
+// }
 
-function logNumber(num: number) {
-  console.log(num);
-  return num;
-}
+// function logNumber(num: number) {
+//   console.log(num);
+//   return num;
+// }
 
 //유니온 타입
-function logText(text: string | number) {
+// function logText(text: string | number) {
+//   console.log(text);
+//   return text;
+// }
+
+// logText(10);
+// logText("하이");
+// logText(true);
+
+function logText<T>(text: T): T {
   console.log(text);
   return text;
 }
 
-logText(10);
-logNumber(10);
-logText("하이");
-logText(true);
+const str = logText<string>("abc");
+str.split("");
+const login = logText<boolean>(true);
 
 //호출 시점에 타입이 정해짐
 // function logText<T>(text: T): T {
@@ -27,3 +35,18 @@ logText(true);
 // }
 
 // logText("하이");
+
+//인터페이스에 제네릭을 선언
+// interface Dropdown {
+//   value: string;
+//   selected: boolean;
+// }
+
+// const obj: Dropdown = { value: 10, selected: false };
+
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const obj: Dropdown<string> = { value: "abc", selected: false };
