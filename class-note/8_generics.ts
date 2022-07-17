4c3934ae8027be63873028f620d63ff8e9b05ff7
myungsangBaek <bms6168@naver.com>
2022-07-17 11:31:42 +0900

    [FEATURE] Limit type to defined type

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -52,12 +52,24 @@ interface Dropdown<T> {
 const obj: Dropdown<string> = { value: "abc", selected: false };
 
 //제네릭의 타입 제한
-function logTextLength<T>(text: T[]): T[] {
-  console.log(text.length);
-  text.forEach(function (text) {
-    console.log(text);
-  });
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach(function (text) {
+//     console.log(text);
+//   });
+//   return text;
+// }
+
+// logTextLength<string>(["h1", "abc"]);
+
+// 제네릭의 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
   return text;
 }
 
-logTextLength<string>(["h1", "abc"]);
+logTextLength({ length: 10 });
